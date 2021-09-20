e9440e9953139276ce44e9d2dda56c2fa7b0d09a
leolousa <leolousa@hotmail.com>
2021-09-20 00:37:12 -0300

    Carregando as permissões concedidas na autenticação

--- src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
+++ src/main/java/com/algaworks/algafood/auth/core/AuthUser.java
@@ -1,7 +1,9 @@
 package com.algaworks.algafood.auth.core;
 
+import java.util.Collection;
 import java.util.Collections;
 
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.User;
 
 import com.algaworks.algafood.auth.domain.Usuario;
@@ -23,8 +25,9 @@ public class AuthUser extends User {
 	private Long userId;
 	private String fullName;
 	
-	public AuthUser(Usuario usuario) {
-		super(usuario.getEmail(), usuario.getSenha(), Collections.emptyList());
+	public AuthUser(Usuario usuario, Collection<? extends GrantedAuthority> authorities) {
+		super(usuario.getEmail(), usuario.getSenha(), authorities);
+		
 		this.userId = usuario.getId();
 		this.fullName = usuario.getNome();
 	}
