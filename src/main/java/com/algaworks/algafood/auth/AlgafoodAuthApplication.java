d12e5941a8bafcf17a81bf8bc27a6a1c0dbb4658
leolousa <leolousa@hotmail.com>
2021-09-26 14:16:32 -0300

    Versão independente da aplicação de autenticação e autorização

--- src/main/java/com/algaworks/algafood/auth/AlgafoodAuthApplication.java
+++ src/main/java/com/algaworks/algafood/auth/AlgafoodAuthApplication.java
@@ -2,7 +2,13 @@ package com.algaworks.algafood.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * Aplicação independente
+ * de autenticação e autorização
+ * 
+ * @author Leonardo
+ *
+ */
 @SpringBootApplication
 public class AlgafoodAuthApplication {
 
